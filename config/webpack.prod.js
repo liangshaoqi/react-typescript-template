64e729addc39a782af03a77e4fa747b942f9dcb1
梁绍奇 <918872153@qq.com>
2021-02-23 09:33:33 +0800

    [update] 测试

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -9,6 +9,7 @@ module.exports = merge(common, {
   output: {
     path: path.resolve(__dirname, '../dist'),
     publicPath: '/',
+    
     filename: 'js/[name].[contenthash].bundle.js'
   },
   plugins: [new CleanWebpackPlugin()],
